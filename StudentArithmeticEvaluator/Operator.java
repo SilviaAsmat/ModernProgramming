00a0d00d0bf14f6c74796f0666f69754657ef4bb
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-07 16:24:05 -0600

    Overrided toString

--- StudentArithmeticEvaluator/Operator.java
+++ StudentArithmeticEvaluator/Operator.java
@@ -2,12 +2,22 @@ public class Operator extends ExpressionToken
 {
     char operator;
 
+    public Operator(char operator) {
+        this.operator = operator;
+    }
+    
+
     public char getOperator() {
         return operator;
     }
 
     public void setOperator(char operator) {
         this.operator = operator;
+    }
+
+    @Override
+    public String toString() {
+        return "" + operator;
     }
     
 }
