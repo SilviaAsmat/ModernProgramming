d7e65d77dd8b163feeda0cfc2c51294bae13a797
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-07 16:22:59 -0600

    Added random expression generating

--- StudentArithmeticEvaluator/ArithmeticQuestion.java
+++ StudentArithmeticEvaluator/ArithmeticQuestion.java
@@ -1,15 +1,47 @@
-public class ArithmeticQuestion implements Question {
 
-    @Override
-    public String getDisplayName() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getDisplayName'");
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
+public class ArithmeticQuestion implements Question 
+{
+    private int numberOfOperators;
+    private ArrayList<ExpressionToken> tokens = new ArrayList<>();
+    private final SecureRandom random = new SecureRandom();
+
+
+    public ArithmeticQuestion(int numberOfOperators) 
+    {
+        this.numberOfOperators = numberOfOperators;
+        initializeTokens();
+    }
+
+    private void initializeTokens()
+    {
+        char[] operators = {'*', '%', '+', '-'};
+        if(numberOfOperators >= 1)
+        {
+            tokens.add(new Operand(random.nextInt(10)));
+            tokens.add(new Operator(operators[random.nextInt(operators.length)]));
+            tokens.add(new Operand(random.nextInt(10)));
+        }
+        for(int i = 1; i < numberOfOperators; i++)
+        {
+            tokens.add(new Operator(operators[random.nextInt(operators.length)]));
+            tokens.add(new Operand(random.nextInt(10)));
+        }
     }
 
     @Override
-    public Boolean isAnswerCorrect(String answer) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isAnswerCorrect'");
+    public String getDisplayName() 
+    {
+        
+        return tokens.toString();
+    }
+
+    @Override
+    public Boolean isAnswerCorrect(String answer) 
+    {
+        return answer.equals("007");
     }
     
 }
