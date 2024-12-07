d3c8838cadf4c6a7fa9662eab864d1c92cbe92b6
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-07 17:26:16 -0600

    Changed node to accept ExpressionToken

--- StudentArithmeticEvaluator/Node.java
+++ StudentArithmeticEvaluator/Node.java
@@ -1,5 +1,13 @@
-public class Node {
+public class Node 
+{
     Node leftChild = null;
     Node rightChild = null;
+    ExpressionToken data = null;
+
+    Node(ExpressionToken data) 
+    {
+        this.data = data;
+    }
+
     
 }
