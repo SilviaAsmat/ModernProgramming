0c6b4ab2ce8aa08c8cb295694723eeb1f6c04116
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-08 10:31:08 -0600

    Added FileOutputStream and filepath to write to

--- StudentArithmeticEvaluator/StudentInteractionLogger.java
+++ StudentArithmeticEvaluator/StudentInteractionLogger.java
@@ -1,29 +1,27 @@
 
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class StudentInteractionLogger 
 {
+    private final FileOutputStream writer;
 
-    private final FileWriter writer;
-
-    public StudentInteractionLogger(String filename) throws IOException
+    public StudentInteractionLogger() 
     {
-        FileWriter tempWriter = null;
+        FileOutputStream tempWriter = null;
         try {
-            tempWriter = new FileWriter(filename, true);
+            tempWriter = new FileOutputStream("Program7-Output.txt", true);
         } catch (IOException e) {
-            System.out.println("Error creating log file: " + e.getMessage());
+            System.out.println("Error creating file: " + e.getMessage());
         }
         writer = tempWriter;
     }
-
     public void log(String message)
     {
         try {
-            writer.write(message + "\n");
+            writer.write((message + "\n").getBytes());
         } catch (IOException e) {
-            System.out.println("Error writing to log file: " + e.getMessage());
+            System.out.println("Error writing to file: " + e.getMessage());
         }
     }
 
@@ -32,7 +30,7 @@ public class StudentInteractionLogger
         try {
             writer.close();
         } catch (IOException e) {
-            System.out.println("Error closing log file: " + e.getMessage());
+            System.out.println("Error closing file: " + e.getMessage());
         }
     }
 }
