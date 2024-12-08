21664973b3886984463fdd85b44f08b38790fc42
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-07 18:00:09 -0600

    Added post order traversal

--- StudentArithmeticEvaluator/BinaryTree.java
+++ StudentArithmeticEvaluator/BinaryTree.java
@@ -1,3 +1,51 @@
-public class BinaryTree {
+public class BinaryTree 
+{
     Node root = null;
+
+    public int evaluate()
+    {
+        return postOrderTraversal(root);
+    }
+
+    private int postOrderTraversal(Node node)
+    {
+        if(node != null)
+        {
+            int leftChildResult = postOrderTraversal(node.leftChild);
+            int rightChildResult = postOrderTraversal(node.rightChild);
+            if(node.data instanceof Operand)
+            {
+                return ((Operand) node.data).getNumber();
+            }
+            else if(node.data instanceof Operator)
+            {
+               Operator operator = (Operator) node.data;
+               switch (operator.getOperator()) {
+                   case '*' -> { return leftChildResult * rightChildResult;
+                    }
+                    case '%' -> { return leftChildResult % rightChildResult;
+                    }
+                    case '+' -> { return leftChildResult + rightChildResult;
+                    }
+                    case '-' -> { return leftChildResult - rightChildResult;
+                    }
+                   default -> throw new AssertionError();
+               }
+
+            }
+            else
+            {
+                throw new IllegalAccessError();
+            }
+            
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+
+
+
 }
