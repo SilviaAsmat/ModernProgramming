2c6703d868fe0a8476e1d5cb80b51d1f91ea5a66
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-08 10:32:57 -0600

    Added student logger

--- StudentArithmeticEvaluator/Program7.java
+++ StudentArithmeticEvaluator/Program7.java
@@ -16,10 +16,13 @@ public class Program7
         QuestionGenerator questionGenerator = new QuestionGenerator();
         LevelManager levelManager = new LevelManager();
         UserFeedbackMessage userFeedbackMessage = new UserFeedbackMessage();
+        StudentInteractionLogger logger = new StudentInteractionLogger();
         // Method to start the program
         System.out.println("Welcome! Let's start learning integer arithmetic expressions.");
         int numOfOperators = levelManager.getNumberOfOperatorsForCurrentLevel();
         Question currentQuestion = questionGenerator.generateQuestion(numOfOperators);
+        logger.log("Question: " + currentQuestion.getDisplayName());
+        
         boolean isFirstTry = true;
         //Beginning of loop, clarify exit conditions
         while (true) 
@@ -27,8 +30,10 @@ public class Program7
             
             System.out.println("Please enter the answer to the following " + levelManager.getLevelDisplayName() + " expression:");
             System.out.println(currentQuestion.getDisplayName());
+            
             String input = scanner.nextLine();
             System.out.println("You entered: " + input);
+            logger.log("Input: " + input);
             boolean correct = currentQuestion.isAnswerCorrect(input);
 
             if (correct) 
@@ -38,22 +43,25 @@ public class Program7
                 boolean userEligibleToMoveNextLevel = levelManager.isUserEligibleForNextLevel();
                 if(userEligibleToMoveNextLevel)
                 {
-                    System.out.println("Would you like to move to next level?(Y/N)");
+                    System.out.println("Would you like to move to next level or exit?(Y/N/Exit)");
+                    logger.log("Would you like to move to next level or exit?(Y/N/Exit)");
                     input = scanner.nextLine();
+                    logger.log("Input: " + input);
                     switch (input) {
                         case "Y" -> {
                             levelManager.moveToNextLevel();
                         }
                         case "N" -> {
                         }
+                        case"Exit" ->{
+                            //find way to exit loop
+                        }
                         default -> System.out.println("Invalid Input");
                     }
                 }
                 numOfOperators = levelManager.getNumberOfOperatorsForCurrentLevel();
-                currentQuestion = questionGenerator.generateQuestion(numOfOperators); // Correct answer, generate a new question
-                // call a method to add if student got it right on the first try - do I need a local variable to track that?s
-                // call a method to determine if level needs to be changed
-                
+                currentQuestion = questionGenerator.generateQuestion(numOfOperators); 
+                logger.log("Question: " + currentQuestion.getDisplayName());
                 isFirstTry = true;
             } 
             else 
