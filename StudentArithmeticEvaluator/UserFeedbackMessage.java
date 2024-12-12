b932a515cd91600788caca6de328612d5a118c6c
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-11 19:21:41 -0600

    Reformatted syntax

--- StudentArithmeticEvaluator/UserFeedbackMessage.java
+++ StudentArithmeticEvaluator/UserFeedbackMessage.java
@@ -1,12 +1,45 @@
+//********************************************************************
+//
+//  Author:        Silvia Asmat
+//
+//  Program #:     Seven
+//
+//  File Name:     Operand.java
+//
+//  Course:        COSC 4301 - Modern Programming
+//
+//  Due Date:      12/11/2024
+//
+//  Java Version:  23.0.1
+//
+//  Instructor:    Prof. Fred Kumi 
+//
+//  Chapter:       Chapter 8 & 9
+//
+//  Description:   This class is responsible for handling the operands
+//
+//********************************************************************
 import java.security.SecureRandom;
 
-public class UserFeedbackMessage {
+public class UserFeedbackMessage 
+{
     private final SecureRandom random = new SecureRandom();
-    
+    //***************************************************************
+    //
+    //  Method:       getCorrectResponse
+    //
+    //  Description:  Returns a random correct response message
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      String
+    //
+    //**************************************************************
     public String getCorrectResponse()
     { 
         // Return a random correct response message
-        String[] responses = {
+        String[] responses = 
+        {
             "Excellent!",
             "Very good!",
             "Nice work!",
@@ -15,10 +48,21 @@ public class UserFeedbackMessage {
         };
         return responses[random.nextInt(responses.length)];
     }
-
+    //***************************************************************
+    //
+    //  Method:       getIncorrectResponse
+    //
+    //  Description:  Returns a random incorrect response message
+    //
+    //  Parameters:   None
+    //
+    //  Returns:      String
+    //
+    //**************************************************************
     public String getIncorrectResponse()
     {
-        String[] incorrectResponses = {
+        String[] incorrectResponses = 
+        {
             "That is incorrect!", 
             "No. Please try again!", 
             "Wrong, Try once more!", 
