53f3213198bf2d71396c9ce00ccf6575f09bc27f
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2024-12-11 19:24:35 -0600

    Fixed final level and exit logic

--- StudentArithmeticEvaluator/Program7.java
+++ StudentArithmeticEvaluator/Program7.java
@@ -9,6 +9,7 @@ public class Program7
     private final UserFeedbackMessage userFeedbackMessage = new UserFeedbackMessage();
     private final StudentInteractionLogger logger = new StudentInteractionLogger();
     private Question currentQuestion;
+    private boolean shouldExit = true;
     public static void main(String[] args) 
     {
         // Main method to start the program
@@ -16,33 +17,30 @@ public class Program7
         program.developerInfo();
         program.startProgram(); 
     }
-
     public void startProgram() 
     {
         String response;
         int numOfOperators = levelManager.getNumberOfOperatorsForCurrentLevel();
         currentQuestion = questionGenerator.generateQuestion(numOfOperators);
         boolean isFirstTry = true;
-        boolean shouldExit = true;
+        
         System.out.println("Welcome! Let's start learning integer arithmetic expressions.");
         while (shouldExit) 
         {   
             boolean correct = getAnswerFromUser();
-
             if (correct) 
             {
                 response = userFeedbackMessage.getCorrectResponse();
                 System.out.println(response);
                 logger.log(response);
                 levelManager.onGotAnswerCorrect(isFirstTry);
-                boolean userEligibleToMoveNextLevel = levelManager.isUserEligibleForNextLevel();
-                if(userEligibleToMoveNextLevel)
+                if(levelManager.isUserEligibleForNextLevel())
                 {
-                    onMoveToNextLevel(shouldExit);
+                    onMoveToNextLevel();
                 }
-                else // Advanced level, can no longer level up
+                else if(levelManager.isFinalLevel())// Advanced level, can no longer level up
                 {
-                    onFinalLevel(shouldExit);
+                    onFinalLevel();
                 }
                 numOfOperators = levelManager.getNumberOfOperatorsForCurrentLevel();
                 currentQuestion = questionGenerator.generateQuestion(numOfOperators); 
@@ -54,37 +52,57 @@ public class Program7
                 isFirstTry = false;
                 // Student tries again, display same question again, until they get it right
             }
-
         }
-        
-        
     }// end of startProgram
-
     public boolean getAnswerFromUser()
     {
         System.out.println("Please enter the answer to the following " + levelManager.getLevelDisplayName() + " expression:");
         System.out.println(currentQuestion.getDisplayName());
         logger.log("Question: " + currentQuestion.getDisplayName());
-        String input = scanner.nextLine();
-        System.out.println("You entered: " + input);
+        int input = getUserIntegerInput();
+        // try()
+        // int input = scanner.nextInt();
+        // scanner.nextLine();
+        // System.out.println("You entered: " + input);
         logger.log("Input: " + input);
         return currentQuestion.isAnswerCorrect(input);
     }
 
-    public void onMoveToNextLevel(boolean shouldExit)
+    public int getUserIntegerInput()
+    {
+        boolean validInput = false;
+        int input = 0;
+        while(!validInput)
+        {
+            if(scanner.hasNextInt())
+            {
+                input = scanner.nextInt();
+                scanner.nextLine();
+                validInput = true;
+            }
+            else
+            {
+                System.out.println("Invalid input. Please enter a valid integer.");
+                scanner.nextLine(); // clear the invalid input
+            }
+        }
+        return input;
+    }
+    public void onMoveToNextLevel()
     {
         System.out.println("Would you like to move to next level or exit?(Y/N/Exit)");
         logger.log("Would you like to move to next level or exit?(Y/N/Exit)");
         String input = scanner.nextLine();
         logger.log("Input: " + input);
-        switch (input.toUpperCase()) {
+        switch (input.toUpperCase()) 
+        {
             case "Y" -> {
                 levelManager.moveToNextLevel();
             }
             case "N" -> {
                 // Do nothing, stay on the current level
             }
-            case "Exit" -> {
+            case "EXIT" -> {
                 // Find way to exit loop
                 shouldExit = false;
                 levelManager.displayLevelStats();
@@ -93,24 +111,28 @@ public class Program7
             default -> System.out.println("Invalid Input");
         }
     }
-
-    public void onFinalLevel(boolean shouldExit)
+    public void onFinalLevel()
     {
-        System.out.println("Would you like to exit?(Exit)");
-        logger.log("Would you like to exit?(Exit)");
+        System.out.println("Would you like to exit?(Y/N)");
+        logger.log("Would you like to exit?(Y/N)");
         String input = scanner.nextLine();
         logger.log("Input: " + input);
         switch (input.toUpperCase()) {
-            case"Exit" -> {
+            case"Y" -> {
                 //find way to exit loop
                 shouldExit = false;
                 levelManager.displayLevelStats();
                 logger.log(levelManager.displayLevelStats());
             }
-            default -> System.out.println("Invalid Input");
+            case "N" -> {
+                // Do nothing, stay on the current level
+            }
+            default -> {
+                System.out.println("Invalid Input");
+                onFinalLevel();
+            }
         }
     }
-
     public void onIncorrectAnswer()
     {
         String response = userFeedbackMessage.getIncorrectResponse();
@@ -118,7 +140,6 @@ public class Program7
         System.out.println(response);
         levelManager.onGotAnswerWrong();
     }
-
     //***************************************************************
     //
     //  Method:       developerInfo
